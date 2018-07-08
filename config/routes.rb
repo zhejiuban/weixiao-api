51b9f155bd9540f2ce019921f449b06b48e496ea
air_wang <air@whit.ah.cn>
2018-07-08 16:43:27 +0800

    fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :ldaps
   namespace :api do
     namespace :v1 do
-      post "hello", to: "root#hello"
+      post "hello", to: "weixin#hello"
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
